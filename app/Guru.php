310037530c48660a17c02afa2ba943c74d7e238d
Andrian Ramadan <ramadanandrian89@gmail.com>
2022-10-07 21:00:36 +0700

    initial commit

---
+++ app/Guru.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Guru extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['id_card', 'nip', 'nama_guru', 'mapel_id', 'kode', 'jk', 'telp', 'tmp_lahir', 'tgl_lahir', 'foto'];
+
+    public function mapel()
+    {
+        return $this->belongsTo('App\Mapel')->withDefault();
+    }
+
+    protected $table = 'guru';
+}
